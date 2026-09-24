d0fcfacac364f659e614a0114d87ffa0799b48ee
shivanirabia-debug <shivanirabia@gmail.com>
2026-09-24 23:22:57 +0500

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
+int main(){
     int hours,attendance,wage,overtime;
     printf("Enter hours worked\n" );
     scanf("%d",&hours);
-    printf("Enter attendance rating(1 to 5)\n");
+    printf("Enter attendance rating(1-5)\n");
     scanf("%d",&attendance);
-    if (hours <= 8) {
-        wage = hours * 500;
+    if(hours<=8){
+        wage=hours*500;
     }
-    else {
-        wage = 8 * 500;
-        if (attendance >= 3) {
-            overtime = hours - 8;
-            wage = wage + (overtime * 500 * 1.5);
+    else{
+        wage=8*500;
+        if (attendance>=3){
+            overtime=hours-8;
+            wage=wage+(overtime*500*1.5);
         }
     }
-    printf("Total daily wage = Rs. %f\n", wage);
+    printf("Total daily wage is %f\n", wage);
 }
