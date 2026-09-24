963619137d22023709feeb3dda69e478e4544d52
shivanirabia-debug <shivanirabia@gmail.com>
2026-09-24 23:10:50 +0500

    Add files via upload

---
+++ task8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    float gpa,income;
+    printf("Enter gpa and family income: \n");
+    scanf("%f%f",&gpa,&income);
+    if(gpa>3.7 && income<50000){
+        printf("Full Scholarship\n");
+    }
+    else if(gpa>3.3 && income<100000){
+        printf("Half Scholarship\n");
+    }
+    else{
+        printf("No Scholarship\n");
+    }
+}
