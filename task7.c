c3cb68a3d1641e7e1e69cbb54a433e0355ed84f5
shivanirabia-debug <shivanirabia@gmail.com>
2026-09-24 23:07:47 +0500

    Update task7.c

--- task7.c
+++ task7.c
@@ -3,12 +3,12 @@
 int main() {
     int plan, minutes;
     float bill;
-    printf("Enter plan number (1-4): ");
+    printf("Enter plan number (1-4): \n");
     scanf("%d", &plan);
     switch (plan) {
         case 1:
             bill = 500;
-            printf("Enter minutes used: ");
+            printf("Enter minutes used: \n");
             scanf("%d", &minutes);
             if (minutes > 1000) {
                 bill = bill + (minutes - 1000) * 2;
@@ -17,24 +17,24 @@ int main() {
             break;
         case 2:
             bill = 800;
-            printf("Enter minutes used: ");
+            printf("Enter minutes used: \n");
             scanf("%d", &minutes);
             if (minutes > 2000) {
                 bill = bill + (minutes - 2000) * 2;
             }
-            printf("Total bill is %f", bill);
+            printf("Total bill is %f\n", bill);
             break;
         case 3:
             bill = 1200;
-            printf("Total bill is %f", bill);
+            printf("Total bill is %f\n", bill);
             break;
         case 4:
-            printf("Enter minutes used: ");
+            printf("Enter minutes used: \n");
             scanf("%d", &minutes);
             bill = minutes * 1;
-            printf("Total bill is %f", bill);
+            printf("Total bill is %f\n", bill);
             break;
         default:
-            printf("Invalid plan number.");
+            printf("Invalid plan number.\n");
     }
 }
